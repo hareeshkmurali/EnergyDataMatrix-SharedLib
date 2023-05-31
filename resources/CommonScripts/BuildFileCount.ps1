05de61f9446c8ff9276fb9081776da8035c87181
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-31 14:19:17 +0530

    Delete resources/CommonScripts directory

--- resources/CommonScripts/BuildFileCount.ps1
+++
@@ -1,4 +0,0 @@
-$workspace = $env:WorkSpace
-$buildpath = $env:BuildPath
-$buildFileCount = (Get-ChildItem -Path "$workspace\\$buildpath" -Recurse -File | Measure-Object).Count
-Write-Output $buildFileCount
