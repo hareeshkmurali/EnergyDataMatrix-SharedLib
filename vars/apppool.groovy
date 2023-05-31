e046aef87e1ec2192da7afdc9a81339051d4239d
hareeshkmurali <44630355+hareeshkmurali@users.noreply.github.com>
2023-05-31 14:19:37 +0530

    Delete vars directory

--- vars/apppool.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-    writeFile file: 'apppool.ps1', text: "${libraryResource 'CommonScripts/Restore-AppPool.ps1'}"
-    bat "powershell -ExecutionPolicy ByPass -File apppool.ps1"
-}
--- vars/deploy.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-    writeFile file: 'deploy.ps1', text: "${libraryResource 'CommonScripts/Deploy-to-IIS.ps1'}"
-    bat "powershell -ExecutionPolicy ByPass -File deploy.ps1"
-}
